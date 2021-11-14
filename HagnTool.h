fab625350e8ad9a39e33266a5d00963e8554af6c
Maximilian Hagn <maximilian.hagn@gmail.com>
2021-11-14 20:02:44 +0100

    Fundamental Changes in hagnlib, arrays now used for storage, new print statistic design, hagn-tool now merges runtimes of decl and other simple statements, better time measurement, better time units, helper functions now part of the hagnlib namespace

---
+++ HagnTool.h
@@ -0,0 +1,8 @@
+//
+// Created by Maximilian Hagn on 14.11.21.
+//
+
+#ifndef CLANG_LLVM_HAGNTOOL_H
+#define CLANG_LLVM_HAGNTOOL_H
+
+#endif //CLANG_LLVM_HAGNTOOL_H
---
+++ lib/DataStorage.h
@@ -0,0 +1,8 @@
+//
+// Created by Maximilian Hagn on 13.11.21.
+//
+
+#ifndef CLANG_LLVM_DATASTORAGE_H
+#define CLANG_LLVM_DATASTORAGE_H
+
+#endif //CLANG_LLVM_DATASTORAGE_H
